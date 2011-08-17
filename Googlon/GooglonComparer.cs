8518e396f6b6198d62bac670f370138a31bf3341
Evandro da Silva Amparo <1426462+evandroamparo@users.noreply.github.com>
2011-08-17 13:06:50 -0300

    Refatora?oes finais

--- Googlon/GooglonComparer.cs
+++ Googlon/GooglonComparer.cs
@@ -5,11 +5,14 @@ namespace Googlon
 {
   class GooglonComparer: IComparer<string>
   {
-    private string _ordemNormal = "";
+    private string _alfabeto;
+    private string _ordemNormal;
 
-    public GooglonComparer()
+    public GooglonComparer(string alfabeto)
     {
-      for (int i = 0; i < GooglonParser.OrdemAlfabetica.Length; i++)
+      _alfabeto = alfabeto;
+      _ordemNormal = "";
+      for (int i = 0; i < alfabeto.Length; i++)
       {
         _ordemNormal = _ordemNormal + char.ConvertFromUtf32(Convert.ToInt32('a') + i);
       }
@@ -23,8 +26,11 @@ namespace Googlon
                y2 = y;
         for (int i = 0; i < _ordemNormal.Length; i++)
         {
-          x2 = x2.Replace(GooglonParser.OrdemAlfabetica[i], char.ToUpper(_ordemNormal[i]));
-          y2 = y2.Replace(GooglonParser.OrdemAlfabetica[i], char.ToUpper(_ordemNormal[i]));
+          if (_alfabeto != null)
+          {
+            x2 = x2.Replace(_alfabeto[i], char.ToUpper(_ordemNormal[i]));
+            y2 = y2.Replace(_alfabeto[i], char.ToUpper(_ordemNormal[i]));
+          }
         }
         return x2.CompareTo(y2);
       }
